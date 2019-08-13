66b1b222dff5334cad37b50613636e4a77176261
Rajesh <rajesh.k.khadka@gmail.com>
2019-08-13 12:44:11 +0545

    change theme

--- src/App.js
+++ src/App.js
@@ -6,19 +6,31 @@ import MovieDetail from './MovieDetail'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import AuthenticatedRoute from './AuthenticatedRoute';
 import AppProvider from './AppContext';
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
+import { purple } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: purple[500] },
+    secondary: { main: '#11cb5f' }
+  }
+})
 
 function App() {
   return (
-    <AppProvider>
-      <Router>
-        <Switch>
-          <Route path="/login" exact component={Login} />
-          <Route path="/" exact component={Login} />
-          <AuthenticatedRoute path="/movies" exact strict component={MovieCategory} />
-          <Route path="/movies/:id" component={MovieDetail} />
-        </Switch>
-      </Router>
-    </AppProvider>
+    <ThemeProvider theme={theme}>
+      <AppProvider>
+        <Router>
+          <Switch>
+            <Route path="/login" exact component={Login} />
+            <Route path="/" exact component={Login} />
+            <AuthenticatedRoute path="/movies" exact strict component={MovieCategory} />
+            <Route path="/movies/:id" component={MovieDetail} />
+          </Switch>
+        </Router>
+      </AppProvider>
+    </ThemeProvider>
   );
 }
 
